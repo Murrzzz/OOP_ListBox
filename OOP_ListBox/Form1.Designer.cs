b01d696281f8244f9cb4e478ad00120206209b39
Bluhzzzz <rogermooresangol@gmail.com>
2021-04-03 07:01:21 +0700

    ListBox in C#

---
+++ OOP_ListBox/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace OOP_ListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.txtChange = new System.Windows.Forms.TextBox();
+            this.txtCash = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnAddtoCart = new System.Windows.Forms.Button();
+            this.btnVoidItems = new System.Windows.Forms.Button();
+            this.btnComputeTotal = new System.Windows.Forms.Button();
+            this.btnComputeChange = new System.Windows.Forms.Button();
+            this.btnNewTrans = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Item:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quantity:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Unit Price:";
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(61, 25);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(168, 22);
+            this.txtItem.TabIndex = 3;
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Location = new System.Drawing.Point(94, 60);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(135, 22);
+            this.txtUnitPrice.TabIndex = 4;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(88, 94);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(141, 22);
+            this.txtQuantity.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Quantity:";
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.ItemHeight = 16;
+            this.ListBox.Location = new System.Drawing.Point(20, 163);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.ScrollAlwaysVisible = true;
+            this.ListBox.Size = new System.Drawing.Size(227, 164);
+            this.ListBox.TabIndex = 7;
+            // 
+            // txtChange
+            // 
+            this.txtChange.Location = new System.Drawing.Point(76, 410);
+            this.txtChange.Name = "txtChange";
+            this.txtChange.Size = new System.Drawing.Size(153, 22);
+            this.txtChange.TabIndex = 13;
+            // 
+            // txtCash
+            // 
+            this.txtCash.Location = new System.Drawing.Point(61, 376);
+            this.txtCash.Name = "txtCash";
+            this.txtCash.Size = new System.Drawing.Size(168, 22);
+            this.txtCash.TabIndex = 12;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(61, 341);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(168, 22);
+            this.txtTotal.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 380);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Cash:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 413);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Change:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 344);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 17);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Total:";
+            // 
+            // btnAddtoCart
+            // 
+            this.btnAddtoCart.Location = new System.Drawing.Point(253, 28);
+            this.btnAddtoCart.Name = "btnAddtoCart";
+            this.btnAddtoCart.Size = new System.Drawing.Size(101, 101);
+            this.btnAddtoCart.TabIndex = 14;
+            this.btnAddtoCart.Text = "Add to Cart";
+            this.btnAddtoCart.UseVisualStyleBackColor = true;
+            this.btnAddtoCart.Click += new System.EventHandler(this.btnAddtoCart_Click);
+            // 
+            // btnVoidItems
+            // 
+            this.btnVoidItems.Location = new System.Drawing.Point(254, 159);
+            this.btnVoidItems.Name = "btnVoidItems";
+            this.btnVoidItems.Size = new System.Drawing.Size(101, 90);
+            this.btnVoidItems.TabIndex = 15;
+            this.btnVoidItems.Text = "Void Selected Items";
+            this.btnVoidItems.UseVisualStyleBackColor = true;
+            this.btnVoidItems.Click += new System.EventHandler(this.btnVoidItems_Click);
+            // 
+            // btnComputeTotal
+            // 
+            this.btnComputeTotal.Location = new System.Drawing.Point(254, 255);
+            this.btnComputeTotal.Name = "btnComputeTotal";
+            this.btnComputeTotal.Size = new System.Drawing.Size(101, 90);
+            this.btnComputeTotal.TabIndex = 16;
+            this.btnComputeTotal.Text = "Compute Total";
+            this.btnComputeTotal.UseVisualStyleBackColor = true;
+            this.btnComputeTotal.Click += new System.EventHandler(this.btnComputeTotal_Click);
+            // 
+            // btnComputeChange
+            // 
+            this.btnComputeChange.Location = new System.Drawing.Point(254, 351);
+            this.btnComputeChange.Name = "btnComputeChange";
+            this.btnComputeChange.Size = new System.Drawing.Size(101, 90);
+            this.btnComputeChange.TabIndex = 17;
+            this.btnComputeChange.Text = "Compute Change";
+            this.btnComputeChange.UseVisualStyleBackColor = true;
+            this.btnComputeChange.Click += new System.EventHandler(this.btnComputeChange_Click);
+            // 
+            // btnNewTrans
+            // 
+            this.btnNewTrans.Location = new System.Drawing.Point(20, 464);
+            this.btnNewTrans.Name = "btnNewTrans";
+            this.btnNewTrans.Size = new System.Drawing.Size(338, 47);
+            this.btnNewTrans.TabIndex = 18;
+            this.btnNewTrans.Text = "New Transaction";
+            this.btnNewTrans.UseVisualStyleBackColor = true;
+            this.btnNewTrans.Click += new System.EventHandler(this.btnNewTrans_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 529);
+            this.Controls.Add(this.btnNewTrans);
+            this.Controls.Add(this.btnComputeChange);
+            this.Controls.Add(this.btnComputeTotal);
+            this.Controls.Add(this.btnVoidItems);
+            this.Controls.Add(this.btnAddtoCart);
+            this.Controls.Add(this.txtChange);
+            this.Controls.Add(this.txtCash);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.txtItem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox ListBox;
+        private System.Windows.Forms.TextBox txtChange;
+        private System.Windows.Forms.TextBox txtCash;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnAddtoCart;
+        private System.Windows.Forms.Button btnVoidItems;
+        private System.Windows.Forms.Button btnComputeTotal;
+        private System.Windows.Forms.Button btnComputeChange;
+        private System.Windows.Forms.Button btnNewTrans;
+    }
+}
+
---
+++ OOP_ListBox/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OOP_ListBox
+{
+
+  
+
+    public partial class Form1 : Form
+    {
+
+        string item;
+        int UnitPrice;
+        int Quantity;
+        int prod;
+        public int[] forTotal = new int[1000];
+        public int[] container = new int[1000];
+        int numArr = 0;
+
+        int total = 0;
+        int cash;
+        int change;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtCash.Text = "0";
+            txtChange.Text = "0";
+            txtTotal.Text = "0";
+        }
+
+        private void btnAddtoCart_Click(object sender, EventArgs e)
+        {
+            
+            item = txtItem.Text;
+            UnitPrice = int.Parse(txtUnitPrice.Text);
+            Quantity = int.Parse(txtQuantity.Text);
+            prod = UnitPrice * Quantity;
+
+            container[numArr] = prod;
+            numArr++;
+            //forTotal[numArr ] = prod;
+          
+            
+            ListBox.Items.Add(item+" @ "+Quantity  +" x "+UnitPrice+".. "+prod );
+             }
+
+        private void btnComputeChange_Click(object sender, EventArgs e)
+        {
+             cash=int.Parse(txtCash.Text);
+             change = int.Parse(txtChange.Text);
+             change = cash - int.Parse (txtTotal .Text );
+             txtChange.Text = change.ToString();
+        }
+      
+      
+        private void btnComputeTotal_Click(object sender, EventArgs e)
+        {
+            //btnVoidItems.PerformClick();
+
+            for (int i = 0; i < numArr  ; i++)
+            {
+                total += container [i];
+            }
+            Console.WriteLine("-------------");
+
+            txtTotal.Text = total.ToString() ;
+            //txtTotal.Text = (total + int.Parse(txtTotal.Text)).ToString();
+        }
+
+       
+
+        private void btnVoidItems_Click(object sender, EventArgs e)
+        {
+
+            if (ListBox.Items.Count >= 0)
+            {
+                string selected = ListBox.Items[ListBox.SelectedIndex].ToString();
+                int ind = ListBox.SelectedIndex;
+                //Console.WriteLine(ListBox.SelectedIndex);
+
+                Console.WriteLine("INdex");
+                Console.WriteLine(ind);
+                ListBox.Items.Remove(selected);
+
+                Console.WriteLine("Items");
+                Console.WriteLine(ListBox.Items.Count);
+
+               
+                for (int i = ind; i <numArr ; i++)
+                {
+                    container[i] = container[i + 1];
+                }
+                numArr--;
+            } 
+        }
+
+        private void btnNewTrans_Click(object sender, EventArgs e)
+        {
+         
+            
+            txtCash.Text = "0";
+            txtChange.Text = "0";
+            txtItem.Text = "";
+            txtQuantity.Text = "";
+            txtTotal.Text = "";
+            txtUnitPrice.Text = "";
+            ListBox.Items.Clear();
+            Array.Clear(container  ,0,container.Length );
+            total = 0;
+        
+
+        }
+    }
+ 
+}
